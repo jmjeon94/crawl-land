a5f4d9983b1de743fbd3f5fcc069439a4fe9ef61
jmjeon <jmjeon3155@gmail.com>
2020-04-14 22:35:49 +0900

    Updates

---
+++ precache-manifest.4313b3e9205a815a3f5db51fdd9df108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf90ea25cdffd36778c75551825f7e6",
+    "url": "/crawl-land/index.html"
+  },
+  {
+    "revision": "8ec69a3c2f2ec86f3106",
+    "url": "/crawl-land/static/css/main.a2a9e283.chunk.css"
+  },
+  {
+    "revision": "c77241078112061820df",
+    "url": "/crawl-land/static/js/2.614c613a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crawl-land/static/js/2.614c613a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ec69a3c2f2ec86f3106",
+    "url": "/crawl-land/static/js/main.1966350e.chunk.js"
+  },
+  {
+    "revision": "af91fdfaa9b3719556ee",
+    "url": "/crawl-land/static/js/runtime-main.e4331941.js"
+  }
+]);
